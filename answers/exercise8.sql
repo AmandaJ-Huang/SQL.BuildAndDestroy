cfb12f874328956a55f1e88e298209226f21298e
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-04 23:00:00 -0500

    exercise8 completed

---
+++ answers/exercise8.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Students (StudentName VARCHAR(255) NOT NULL, Address VARCHAR(255) NOT NULL, City VARCHAR(255) NOT NULL, PostalCode VARCHAR(255) NOT NULL, COUNTRY VARCHAR(255) NOT NULL);
+
+INSERT INTO myNewDB.Students (StudentName, Address, City, PostalCode, Country) VALUES ('Jane Doe', '57 Union St', 'Glasgow', 'G13RB', 'Scotland');
